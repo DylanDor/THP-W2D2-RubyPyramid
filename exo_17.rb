0276313ee04c8bc2798e64faabf6d2b9511c2a8e
Dylan D <dore.dylan31@gmail.com>
2022-10-04 15:45:33 +0200

    new exo17

--- exo_17.rb
+++ exo_17.rb
@@ -2,9 +2,13 @@ puts "Construit une pyramide ! Combien d'étages ?"
 print "> "
 rows = gets.chomp.to_i
 
-for row in 1..rows
-	(rows-row).times {print " "}
-    row.times {print "#"}
-    (row-1).times {print "#"}
-	puts
+if rows <= 25
+    for row in 1..rows
+        (rows-row).times {print " "}
+        row.times {print "#"}
+        (row-1).times {print "#"}
+        puts
+    end
+else 
+    puts "c cho frère"
 end
